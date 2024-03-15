30b30538fb8d450485fe87b214613b8256caaefc
Natalia Taira <nataliagtaira@gmail.com>
2024-03-15 01:53:21 -0300

    Submission of program.

---
+++ ATIVIDADE-B1-2/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char  frase[100];
+    char  *palavra;
+    int   tamPalavra;
+    int   palindromo;
+    int   i;
+
+    palindromo = 0;
+
+    printf("-------------------\n");
+    printf("Digite uma frase: \n");
+    fgets(frase, sizeof(frase), stdin);
+
+    palavra = strtok(frase, " \n");
+
+    printf("Palavras palindromo na frase: \n");
+
+    while (palavra != NULL)
+    {
+        tamPalavra = strlen(palavra);
+
+        for(i = 0; i < tamPalavra / 2; i++)
+        {
+            if (palavra[i] == palavra[tamPalavra - i - 1])
+            {
+                palindromo = 1;
+            }
+            else
+            {
+                palindromo = 0;
+                break;
+            }
+        }
+
+        if(palindromo == 1)
+            printf("%s \n", palavra);
+
+        palavra = strtok(NULL, " \n");
+    }
+
+    printf("-------------------");
+    return 0;
+}
