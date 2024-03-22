684a0c02c302b9727efa5c5fe45ba8d86d95b310
Natalia Taira <nataliagtaira@gmail.com>
2024-03-22 17:09:22 -0300

    Fix tabuleiro data type

--- ATIVIDADE-B1-3/atividade-b1-3.c
+++ ATIVIDADE-B1-3/atividade-b1-3.c
@@ -2,21 +2,15 @@
 
 #define LIN 8
 #define COL 8
-#define ID "TCBDRP"
-typedef enum cor
-{
-    BRANCO,
-    PRETO
-}   Cor;
 
 typedef struct  peca
 {
-    char    *cor;
-    char    *id;
-    char    *pos;
+    char    cor;
+    char    *nome;
+    char     num;
 }   Peca;
 
-void    camposVazios(char tabuleiro[LIN][COL])
+void    camposVazios(Peca tabuleiro[LIN][COL])
 {
     int     posLin = 2;
     int     posCol;
@@ -26,40 +20,49 @@ void    camposVazios(char tabuleiro[LIN][COL])
         posCol = 0;
         while (posCol < COL)
         {
-            tabuleiro[posLin][posCol] = 'X';
+            tabuleiro[posLin][posCol].cor = '-';
+            tabuleiro[posLin][posCol].nome = "X";
+            tabuleiro[posLin][posCol].num = '-';
             posCol++;
         }
         posLin++;
     }
 }
 
-void    printTabuleiro(char tabuleiro[LIN][COL])
-{
-    int     posLin = 0;
-    int     posCol;
-    int     num = 1;
+// void    inserirPecas(Peca *tabuleiro[LIN][COL])
+// {
+//     if ()
+// }
+
+// void    printTabuleiro(Peca tabuleiro[LIN][COL])
+// {
+//     int     posLin = 0;
+//     int     posCol;
+//     int     num = 1;
     
 
-    while (posLin < LIN)
-    {
-        posCol = 0;
-        printf("%i ", num++);
-        while (posCol < COL)
-        {
-            printf(" %c ", tabuleiro[posLin][posCol]);
-            posCol++;
-        }
-        printf("\n");
-        posLin++;
-    }
-    printf("   A  B  C  D  E  F  G  H\n");
-}
+//     while (posLin < LIN)
+//     {
+//         posCol = 0;
+//         printf("%i ", num++);
+//         while (posCol < COL)
+//         {
+//             printf("%c%s%c", tabuleiro[posLin][posCol].cor, tabuleiro[posLin][posCol].nome, tabuleiro[posLin][posCol].num);
+//             posCol++;
+//         }
+//         printf("\n");
+//         posLin++;
+//     }
+//     printf("   A  B  C  D  E  F  G  H\n");
+// }
 
 int main(void)
 {
-    char    tabuleiro[LIN][COL];
+    Peca    tabuleiro[LIN][COL];
 
     camposVazios(tabuleiro);
-    printTabuleiro(tabuleiro);
+    // inserirPecas(tabuleiro);
+    // printTabuleiro(tabuleiro);
+    printf("%c%s%c", tabuleiro[3][0].cor, tabuleiro[3][0].nome, tabuleiro[3][0].num);
     return (0);
 }
