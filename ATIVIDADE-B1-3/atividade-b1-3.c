b4c55fcf0170aaf2ccb67dcc4c48526ed7934c99
Natalia Taira <nataliagtaira@gmail.com>
2024-04-01 16:15:31 -0300

    Desmembrado em funções auxiliares.

--- ATIVIDADE-B1-3/atividade-b1-3.c
+++ ATIVIDADE-B1-3/atividade-b1-3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define LIN 8
 #define COL 8
@@ -6,14 +7,14 @@
 typedef struct  peca
 {
     char    cor;
-    char    *nome;
-    char     num;
+    char    nome;
+    char    num;
 }   Peca;
 
 void    camposVazios(Peca tabuleiro[LIN][COL])
 {
-    int     posLin = 2;
-    int     posCol;
+    int posLin = 2;
+    int posCol;
 
     while (posLin < LIN - 2)
     {
@@ -21,7 +22,7 @@ void    camposVazios(Peca tabuleiro[LIN][COL])
         while (posCol < COL)
         {
             tabuleiro[posLin][posCol].cor = '-';
-            tabuleiro[posLin][posCol].nome = "X";
+            tabuleiro[posLin][posCol].nome = 'X';
             tabuleiro[posLin][posCol].num = '-';
             posCol++;
         }
@@ -29,40 +30,91 @@ void    camposVazios(Peca tabuleiro[LIN][COL])
     }
 }
 
-// void    inserirPecas(Peca *tabuleiro[LIN][COL])
-// {
-//     if ()
-// }
+void    defineCorPecas(Peca tabuleiro[LIN][COL])
+{
+    int posLin = 0;
+    int posCol;
 
-// void    printTabuleiro(Peca tabuleiro[LIN][COL])
-// {
-//     int     posLin = 0;
-//     int     posCol;
-//     int     num = 1;
+    while (posLin < LIN)
+    {
+        posCol = 0;
+        while (posCol < COL)
+        {
+            if (posLin == 0 || posLin == 1)
+                tabuleiro[posLin][posCol].cor = 'B';
+            else if (posLin == 6 || posLin == 7)
+                tabuleiro[posLin][posCol].cor = 'P';
+            posCol++;
+        }
+        posLin++;
+    }
+}
+
+void    posicaoPecas(Peca tabuleiro [LIN][COL])
+{
+    int     posLin = 0;
+    int     posCol;
+    char    pecas[8] = {'T', 'C', 'B', 'D', 'R', 'B', 'C', 'T'};
+    char    num[8] = {'1', '1', '1', '-', '-', '2', '2', '2'};
+    char    numP[8] = {'1', '2', '3', '4', '5', '6', '7', '8'};
+
+    while (posLin < LIN)
+    {
+        posCol = 0;
+        while (posCol < COL)
+        {
+            if (posLin == 0 || posLin == 7)
+            {
+                tabuleiro[posLin][posCol].nome = pecas[posCol];
+                tabuleiro[posLin][posCol].num = num[posCol];
+            }
+            else if (posLin == 1 || posLin == 6)
+            {
+                tabuleiro[posLin][posCol].nome = 'P';
+                tabuleiro[posLin][posCol].num = numP[posCol];
+            }
+            posCol++;
+        }
+        posLin++;
+    }
+}
+
+void    inserirPecas(Peca tabuleiro[LIN][COL])
+{
+    defineCorPecas(tabuleiro);
+    posicaoPecas(tabuleiro);
+}
+
+void    printTabuleiro(Peca tabuleiro[LIN][COL])
+{
+    int     posLin = 0;
+    int     posCol;
+    int     num = 1;
     
 
-//     while (posLin < LIN)
-//     {
-//         posCol = 0;
-//         printf("%i ", num++);
-//         while (posCol < COL)
-//         {
-//             printf("%c%s%c", tabuleiro[posLin][posCol].cor, tabuleiro[posLin][posCol].nome, tabuleiro[posLin][posCol].num);
-//             posCol++;
-//         }
-//         printf("\n");
-//         posLin++;
-//     }
-//     printf("   A  B  C  D  E  F  G  H\n");
-// }
+    while (posLin < LIN)
+    {
+        posCol = 0;
+        printf("%i ", num++);
+        while (posCol < COL)
+        {
+            printf(" %c%c%c ", tabuleiro[posLin][posCol].cor, \
+            tabuleiro[posLin][posCol].nome, \
+            tabuleiro[posLin][posCol].num);
+            posCol++;
+        }
+        printf("\n");
+        posLin++;
+    }
+    printf("    A    B    C    D    E    F    G    H\n");
+}
 
 int main(void)
 {
     Peca    tabuleiro[LIN][COL];
 
     camposVazios(tabuleiro);
-    // inserirPecas(tabuleiro);
-    // printTabuleiro(tabuleiro);
-    printf("%c%s%c", tabuleiro[3][0].cor, tabuleiro[3][0].nome, tabuleiro[3][0].num);
+    inserirPecas(tabuleiro);
+    printTabuleiro(tabuleiro);
     return (0);
 }
