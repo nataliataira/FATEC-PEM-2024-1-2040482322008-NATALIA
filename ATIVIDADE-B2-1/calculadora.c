cdefd79116b2c1a2b9cc1d6bcfc909f3c78ac4c5
Natalia Taira <nataliagtaira@gmail.com>
2024-04-26 21:26:59 -0300

    Bugs resolvidos.

--- ATIVIDADE-B2-1/calculadora.c
+++ ATIVIDADE-B2-1/calculadora.c
@@ -2,25 +2,21 @@
 #include <ctype.h>
 #include <float.h>
 
-void    calcular(void);
-float   soma(float operador1, float operador2);
-float   subtracao(float operador1, float operador2);
-float multiplicacao(float operador1, float operador2);
-float   divisao(float operador1, float operador2);
+int calcular(void);
+void calcularOperacao(char operacao, float operadores[2]);
 
 int main(void)
 {
-    calcular();
-    return (0);
+    while (calcular()) {}
+    return 0;
 }
 
-void    calcular(void)
+int calcular(void)
 {
-    char    operacao = '0';
+    char operacao = '0';
     int c;
-    int     valido = 0;
-    float   operadores[2] = {0, 0};
-    float resultado = 0;
+    int valido = 0;
+    float operadores[2] = {0, 0};
 
     printf("\nCALCULADORA!\n\n");
 
@@ -30,7 +26,7 @@ void    calcular(void)
         printf("(1)Soma           (4)Divisão\n");
         printf("(2)Subtração      (S)Sair\n(3)Multiplicação\n\n");
         printf("Escolha a operação desejada: ");
-        scanf("%c", &operacao);
+        scanf(" %c", &operacao); // Adicionando espaço antes de %c para ignorar espaços em branco
 
         operacao = tolower(operacao);
 
@@ -40,7 +36,7 @@ void    calcular(void)
             continue;
         }
         else if (operacao == 's')
-            return (0);
+            return 0;
 
         while (valido == 0)
         {
@@ -58,120 +54,39 @@ void    calcular(void)
             valido = 1;
         }
 
-        switch (operacao)
-        {
-            case '1':
-            {
-                resultado = soma(operadores[0], operadores[1]);
-                printf("%2.f + %2.f = %2.f\n\n", operadores[0], operadores[1], resultado);
-                break;
-            }
-            case '2':
-            {
-                resultado = subtracao(operadores[0], operadores[1]);
-                printf("%2.f - %2.f = %2.f\n\n", operadores[0], operadores[1], resultado);
-                break;
-            }
-            case '3':
-            {
-                resultado = multiplicacao(operadores[0], operadores[1]);
-                printf("%2.f * %2.f = %2.f\n\n", operadores[0], operadores[1], resultado);
-                break;
-            }
-            case '4':
-            {
-                resultado = divisao(operadores[0], operadores[1]);
-                printf("%2.f / %2.f = %2.f\n\n", operadores[0], operadores[1], resultado);
-                break;
-            }
-            case 's':
-                break;
-            default:
-                printf("Operação inválida.\n");
-        }
+        calcularOperacao(operacao, operadores);
         valido = 0;
     }
+
+    return 1; // Retorna 1 para indicar que o usuário deseja continuar
 }
 
-    
-
-float   soma(float operador1, float operador2)
+void calcularOperacao(char operacao, float operadores[2])
 {
-    float floatmax = FLT_MAX;
-    float floatmin = FLT_MIN;
+    float resultado;
 
-    if (operador1 > 0 && operador2 > 0 && operador1 > floatmax - operador2)
+    switch (operacao)
     {
-        printf("Resultado da operação é grande demais, será mostrado o maior valor possível.\n");
-        return floatmax;
-    }
-    else if (operador1 < 0 && operador2 < 0 && operador1 < floatmin - operador2)
-    {
-        printf("Resultado da operação é pequeno demais, será mostrado o menor valor possível.\n");
-        return floatmin;
-    }
-    else
-    {
-        return operador1 + operador2;
-    }
-}
-
-float   subtracao(float operador1, float operador2)
-{
-    float floatmax = FLT_MAX;
-
-    if (operador1 > 0 && operador2 < 0 && operador1 > floatmax + operador2)
-    {
-        printf("Resultado da operação é grande demais, será mostrado o menor valor possível.\n");
-        return floatmax;
-    }
-    else if (operador1 < 0 && operador2 > 0 && operador1 < -floatmax - operador2)
-    {
-        printf("Resultado da operação é pequeno demais, será mostrado o menor valor possível.\n");
-        return -floatmax;
-    }
-    else
-    {
-        return operador1 - operador2;
-    }
-}
-
-float multiplicacao(float operador1, float operador2)
-{
-    float floatmax = FLT_MAX;
-    float floatmin = FLT_MIN;
-
-    if (operador1 != 0 && operador2 != 0 && operador1 > floatmax / operador2)
-    {
-        printf("Resultado da operação é grande demais, será mostrado o maior valor possível.\n");
-        return floatmax;
-    }
-    else if (operador1 != 0 && operador2 != 0 && operador1 < floatmin / operador2)
-    {
-        printf("Resultado da operação é pequeno demais, será mostrado o menor valor possível.\n");
-        return floatmin;
-    }
-    else
-    {
-        return operador1 * operador2;
-    }
-}
-
-float   divisao(float operador1, float operador2)
-{
-    float floatmax = FLT_MAX;
-    float floatmin = FLT_MIN;
-
-    if ((operador1 > 0 && operador2 < 0 && operador1 > -floatmin / operador2) ||
-        (operador1 < 0 && operador2 > 0 && operador1 < -floatmin / operador2) ||
-        (operador1 > 0 && operador2 > 0 && operador1 > floatmax / operador2) ||
-        (operador1 < 0 && operador2 < 0 && operador1 < floatmax / operador2))
-    {
-        printf("Resultado da operação é grande demais, será mostrado o maior valor possível.\n");
-        return floatmax;
-    }
-    else
-    {
-        return operador1 / operador2;
+    case '1':
+        resultado = operadores[0] + operadores[1];
+        printf("%2.f + %2.f = %2.f\n\n", operadores[0], operadores[1], resultado);
+        break;
+    case '2':
+        resultado = operadores[0] - operadores[1];
+        printf("%2.f - %2.f = %2.f\n\n", operadores[0], operadores[1], resultado);
+        break;
+    case '3':
+        resultado = operadores[0] * operadores[1];
+        printf("%2.f * %2.f = %2.f\n\n", operadores[0], operadores[1], resultado);
+        break;
+    case '4':
+        resultado = operadores[0] / operadores[1];
+        printf("%2.f / %2.f = %2.f\n\n", operadores[0], operadores[1], resultado);
+        break;
+    case 's':
+        printf("Saindo...\n");
+        break;
+    default:
+        printf("Operação inválida.\n");
     }
 }
